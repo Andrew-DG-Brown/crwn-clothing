c81215b497aaef844e1dfe80d6f7c05447c44316
Andrew-DG-Brown <andrew.dg.brown@gmail.com>
2022-07-09 14:54:07 -0400

    Added naviagtion links to main directory page

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -5,38 +5,54 @@ import {
   BackgroundImage,
 } from "./directory-item.styles";
 
-const DirectoryItem = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "Womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "Mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
+import { useNavigate } from "react-router-dom";
 
-  return categories.map(({ id, title, imageUrl }) => {
+const categories = [
+  {
+    id: 1,
+    title: "Hats",
+    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    route: "shop/hats",
+  },
+  {
+    id: 2,
+    title: "Jackets",
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    route: "shop/jackets",
+  },
+  {
+    id: 3,
+    title: "Sneakers",
+    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    route: "shop/sneakers",
+  },
+  {
+    id: 4,
+    title: "Womens",
+    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    route: "shop/womens",
+  },
+  {
+    id: 5,
+    title: "Mens",
+    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    route: "shop/mens",
+  },
+];
+
+/* 
+    Refactor this to intake the categories data, watch Section 11, episode "last touches"
+*/
+
+const DirectoryItem = () => {
+  const navigate = useNavigate();
+
+  return categories.map(({ id, title, imageUrl, route }) => {
+    const onNavigateHandler = () => {
+      navigate(route);
+    };
     return (
-      <DirectoryItemContainer key={id}>
+      <DirectoryItemContainer key={id} onClick={onNavigateHandler}>
         <BackgroundImage imageUrl={imageUrl}>
           <Body>
             <h2>{title}</h2>
